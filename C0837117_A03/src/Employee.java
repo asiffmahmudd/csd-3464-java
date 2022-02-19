9f1dd6029e081181823eb95c093c8301a02d78ea
Asif Mahmud <asifmahmud3472@gmail.com>
2022-02-18 22:15:40 -0500

    assignment 3, question 1 done

--- C0837117_A03/src/Employee.java
+++ C0837117_A03/src/Employee.java
@@ -3,7 +3,9 @@
  ID: C0837117
  Date
 */
-import java.util.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Employee {
 	private static int numEmployees;
@@ -13,42 +15,62 @@ public class Employee {
 	// Two Parameter Constructor
 	public Employee(String name, String dateOfHire)
 	{
-	
+		numEmployees++;
+		this.empNum = numEmployees;
+		
+		String[] date = dateOfHire.split("/");
+		int year = Integer.parseInt(date[0]);
+		int month = Integer.parseInt(date[1]);
+		int day = Integer.parseInt(date[2]);
+		this.dateOfHire = new GregorianCalendar(year, month, day); // sets calendar object to specific year, month, day 
+		
+		this.name = new Name(name);
+		
 	}
 	// Copy Constructor
 	public Employee(Employee empToCopy)
 	{
-	
+		numEmployees++;
+		this.empNum = numEmployees;
+		
+		this.name = new Name(empToCopy.name);
+		this.dateOfHire = (Calendar)(empToCopy.dateOfHire.clone());
+		
 	}
 	// Mutator - DateOfHire
 	public void setDateOfHire(int year, int month, int day)
 	{
-	
+		dateOfHire.set(Calendar.DATE, day);
+		dateOfHire.set(Calendar.MONTH, month);
+		dateOfHire.set(Calendar.YEAR, year);
 	} 
 	// Accessor - DateOfHire
 	public String getDateOfHire()
 	{
-		return this.dateOfHire.toString();
+		SimpleDateFormat sdf = new SimpleDateFormat("MMMM dd, YY");
+		return sdf.format(dateOfHire.getTime());
 	} 
 	// Mutator - DateOfHireDay
 	public void setDateOfHireDay(int day)
 	{
-	
+		dateOfHire.set(Calendar.DATE, day);
 	}
 	// Mutator - DateOfHireMonth
 	public void setDateOfHireMonth(int month)
 	{
-	
+		dateOfHire.set(Calendar.MONTH, month);
 	}
 	// Mutator - DateOfHireYear
 	public void setDateOfHireYear(int year)
 	{
-	
+		dateOfHire.set(Calendar.YEAR, year);
 	}
 	// Mutator - Name
 	public void setName(String first, String middle, String last)
 	{
-	
+		this.name.setFirstName(first);
+		this.name.setMiddleName(middle);
+		this.name.setLastName(last);
 	}
 	// Accessor – Name
 	public String getName()
@@ -58,45 +80,64 @@ public class Employee {
 	// Mutator - FirstName
 	public void setFirstName(String first)
 	{
-	
+		this.name.setFirstName(first);
 	}
 	// Accessor – FirstName
 	public String getFirstName()
 	{
-		return this.getFirstName();
+		return this.name.getFirstName();
 	} 
 	// Mutator - MiddleName
 	public void setMiddleName(String middle)
 	{
-		
+		this.name.setMiddleName(middle);
 	}
 	// Accessor – MiddleName
 	public String getMiddleName()
 	{
-		return this.name.getMiddlName();
+		if(this.name.getMiddlName() == null) {
+			return "None";
+		}
+		else {
+			return this.name.getMiddlName();
+		}
 	} 
 	// Mutator - LastName
 	public void setLastName(String last)
 	{
-	
+		this.name.setLastName(last);
 	}
 	// Accessor – LastName
 	public String getLastName()
 	{
-		return this.getLastName();
+		return this.name.getLastName();
 	} 
 	// toString method
 	@Override
 	public String toString()
 	{
-		
+		return this.empNum + " : " + this.name.toString() + " - " + this.getDateOfHire(); 
 	}
 	 
 	// equals method
 	@Override
 	public boolean equals(Object o)
 	{
-	
+        if (o == this) {
+            return true;
+        }
+ 
+        if (!(o instanceof Employee)) {
+            return false;
+        }
+        Employee e = (Employee) o;
+        
+        if(e.empNum != this.empNum) {
+        	return false;
+        }
+         
+        // Compare the data members and return accordingly
+        return name.equals(e.name)  && dateOfHire.equals(e.dateOfHire);
 	}
 }
 
---
+++ C0837117_A03/src/EmployeeDemo.java
@@ -0,0 +1,27 @@
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee("Asif Mahmud", "22/2/2");
+		Employee e2 = new Employee("Not Asif Mahmud", "21/3/7");
+		System.out.println(e1.equals(e2));
+		System.out.println(e1.getDateOfHire());
+		System.out.println(e1.toString());
+		System.out.println(e2.toString());
+		System.out.println(e1.getFirstName());
+		System.out.println(e1.getLastName());
+		System.out.println(e1.getMiddleName());
+		System.out.println(e1.getName());
+		e1.setFirstName("Brian");
+		e1.setLastName("Tat");
+		e1.setMiddleName("Chou");
+		e1.setDateOfHire(2021, 10, 9);
+		System.out.println(e1.toString());
+		e2.setDateOfHireDay(10);
+		e2.setDateOfHireMonth(6);
+		e2.setDateOfHireYear(2022);
+		e2.setName("First", "middle", "last");
+		System.out.println(e2.toString());
+	}
+
+}
